28c38052728fdf9bac7a8921cb3114cd4ccdfb78
kfly8 <kfly@cpan.org>
2021-03-15 19:43:01 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 
 # NAME
 
-Sub::WrapInType::Attribute - It's new $module
+Sub::WrapInType::Attribute - attribute for Sub::WrapInType
 
 # SYNOPSIS
 
 ```perl
 use Sub::WrapInType::Attribute;
+use Types::Standard -types;
+
+sub hello :WrapSub([Str] => Str) {
+    my $message = shift;
+    return "HELLO $message";
+}
+
+hello('world!!'); # => HELLO world!!
+my $code = \&hello; # => Sub::WrapInType object
 ```
 
 # DESCRIPTION
 
-Sub::WrapInType::Attribute is ...
+This module provides attribute for Sub::WrapInType, which makes it easier to check during the compilation phase.
+
+# SEE ALSO
+
+[Sub::WrapInType](https://metacpan.org/pod/Sub%3A%3AWrapInType)
 
 # LICENSE
 
--- lib/Sub/WrapInType/Attribute.pm
+++ lib/Sub/WrapInType/Attribute.pm
@@ -1,11 +1,59 @@
 package Sub::WrapInType::Attribute;
-use 5.008001;
+use v5.10.0;
 use strict;
 use warnings;
 
 our $VERSION = "0.01";
 
+use Attribute::Handlers;
+use B::Hooks::EndOfScope;
+use Sub::WrapInType::Attribute::Installer;
+use namespace::autoclean;
 
+my $DEFAULT_CHECK = !!($ENV{SUB_WRAPINTYPE_ATTRIBUTE_CHECK} // 1);
+my %CHECK;
+
+sub import {
+    my $class = shift;
+    my %args = @_;
+
+    my $pkg = $args{pkg} ? $args{pkg} : scalar caller;
+    $CHECK{$pkg} = !!$args{check} if exists $args{check};
+    {
+        # allow importing package to use attribute
+        no strict 'refs';
+        push @{"${pkg}::ISA"}, $class;
+    }
+    return;
+}
+
+sub WrapSub :ATTR(CODE,BEGIN) {
+    my ($pkg, @args) = @_;
+
+    my $opts = {
+        check => $CHECK{$pkg} // $DEFAULT_CHECK,
+        skip_invocant => 0,
+    };
+
+    on_scope_end {
+        Sub::WrapInType::Attribute::Installer->install($opts, $pkg, @args);
+    };
+    return;
+}
+
+sub WrapMethod :ATTR(CODE,BEGIN) {
+    my ($pkg, @args) = @_;
+
+    my $opts = {
+        check => $CHECK{$pkg} // $DEFAULT_CHECK,
+        skip_invocant => 1,
+    };
+
+    on_scope_end {
+        Sub::WrapInType::Attribute::Installer->install($opts, $pkg, @args);
+    };
+    return;
+}
 
 1;
 __END__
@@ -14,15 +62,28 @@ __END__
 
 =head1 NAME
 
-Sub::WrapInType::Attribute - It's new $module
+Sub::WrapInType::Attribute - attribute for Sub::WrapInType
 
 =head1 SYNOPSIS
 
     use Sub::WrapInType::Attribute;
+    use Types::Standard -types;
+
+    sub hello :WrapSub([Str] => Str) {
+        my $message = shift;
+        return "HELLO $message";
+    }
+
+    hello('world!!'); # => HELLO world!!
+    my $code = \&hello; # => Sub::WrapInType object
 
 =head1 DESCRIPTION
 
-Sub::WrapInType::Attribute is ...
+This module provides attribute for Sub::WrapInType, which makes it easier to check during the compilation phase.
+
+=head1 SEE ALSO
+
+L<Sub::WrapInType>
 
 =head1 LICENSE
 
